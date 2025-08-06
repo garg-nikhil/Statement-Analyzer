b56224f16dc64cdc0d907a11c27ac89bcaafe9c4
Nikhil Garg <nikhilgarg855@gmail.com>
2025-08-07 01:16:07 +0530

    cors fixes

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, request, jsonify
 import tempfile
 from utils.pdf_extractor import extract_transactions
 from utils.parser import segregate_by_vendor_type
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["https://garg-nikhil.github.io"])
 
 @app.route('/process', methods=['POST'])
 def process_pdf():
